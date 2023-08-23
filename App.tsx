448e9f14618371426b614395c75896c5412a3861
jinsunee <jsunee.dev@email.com>
2023-08-23 14:37:12 +0900

    feat: firebase config

--- App.tsx
+++ App.tsx
@@ -1,6 +1,13 @@
+import firebase from '@react-native-firebase/app';
 import React from 'react';
+import {firebaseConfig} from './firebaseConfig';
 import Navigators from './src/components/navigators';
 import Providers from './src/components/providers';
+
+const config = {
+  name: 'SECONDARY_APP',
+};
+firebase.initializeApp(firebaseConfig, config);
 
 export default function App() {
   return (
