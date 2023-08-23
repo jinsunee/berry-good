380b17731067e828664b1ce74a69f565238e697e
jinsunee <jsunee.dev@email.com>
2023-08-23 14:33:19 +0900

    feat: firebase ios & android 설정

--- src/components/navigators/RootStackNavigator.tsx
+++ src/components/navigators/RootStackNavigator.tsx
@@ -1,24 +1,56 @@
+import firebase from '@react-native-firebase/app';
+import auth from '@react-native-firebase/auth';
 import {
   createNativeStackNavigator,
   NativeStackNavigationProp,
 } from '@react-navigation/native-stack';
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useRecoilState} from 'recoil';
+import {userAtom} from '../../states';
 import FirstStack from './FirstStackNavigator';
 import HomeStack from './HomeStackNavigator';
 
-export type HomeStackParamList = {
+export type RootStackParamList = {
   FirstStack: undefined;
   HomeStack: undefined;
 };
 
-export type HomeStackNavigationType = NativeStackNavigationProp<
-  HomeStackParamList,
+export type RootStackNavigationType = NativeStackNavigationProp<
+  RootStackParamList,
   'HomeStack'
 >;
 
-const Stack = createNativeStackNavigator<HomeStackParamList>();
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
-export default function HomeStackNavigator() {
+export default function RootStackNavigator() {
+  const [_, setCurrentUser] = useRecoilState(userAtom);
+
+  useEffect(() => {
+    if (firebase.apps.length === 0) {
+      return;
+    }
+    const unsubscribe = auth().onAuthStateChanged(async user => {
+      if (user) {
+        // Fetch user information from the API
+        // const response = await fetch('http://localhost:3000/api/users', {
+        //   method: 'POST',
+        //   headers: {
+        //     'Content-Type': 'application/json',
+        //   },
+        //   body: JSON.stringify({userId: user.uid}),
+        // });
+        // const data = await response.json();
+        // // Now, you can set this data to the recoil state or handle it as required.
+        // setCurrentUser(data);
+      } else {
+        // User is signed out.
+        setCurrentUser(null);
+      }
+    });
+
+    return () => unsubscribe();
+  }, [setCurrentUser]);
+
   return (
     <Stack.Navigator>
       <Stack.Screen
---
+++ src/utils/colors.ts
@@ -0,0 +1,7 @@
+const colors = {
+  white: '#fff',
+  dark: '#474747',
+  secondary: ['#F2F2F2', '#CBCBCB', '#8B8B8B'],
+};
+
+export {colors};
